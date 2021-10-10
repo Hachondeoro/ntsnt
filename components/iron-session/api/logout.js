8755bf095f0f11e7c520a80cfa1ae6e75d143898
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2021-10-10 12:31:23 +0930

    removed api auth

--- components/iron-session/api/logout.js
+++ components/iron-session/api/logout.js
@@ -1,4 +1,4 @@
-import withSession from "../../lib/session";
+import withSession from "@components/iron-session/session";
 
 export default withSession(async (req, res) => {
   req.session.destroy();
